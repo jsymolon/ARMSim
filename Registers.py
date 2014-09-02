7ad547c243a3ac8183cb71da7f89c536f1896fe1
jim symolon <jsymolon@gmail.com>
2014-09-02 19:30:25 -0400

    move the model/string build functions out of the view

--- Registers.py
+++ Registers.py
@@ -5,6 +5,7 @@ import wx.lib.mixins.inspection
 import logging
 import globals
 import ARMCPU
+import utils
 
 ###########################################################################
 ## Handle the Register window
@@ -22,70 +23,6 @@ class Registers(wx.Panel):
         self.fheight = fontsz.GetHeight()
         self.Bind( wx.EVT_PAINT, self.onPaint )
 
-    def buildRegValString(self, i):
-        out = "R"+str("%02d"%i)+":"+str("%08x" % globals.regs[i])
-        out += str(" %08x" % globals.regs[i])
-        if (i == globals.SP):
-            out += " (sp)"
-        if (i == globals.LINK):
-            out += " (lr)"
-        if (i == globals.PC):
-            out += " (pc)"
-        if (i == globals.CPSR):
-            out += " (cpsr)"
-            if (globals.regs[i] & globals.NEGATIVEBIT == globals.NEGATIVEBIT):
-                cpsr = " N"
-            else:
-                cpsr = " n"
-            if (globals.regs[i] & globals.ZEROBIT == globals.ZEROBIT):
-                cpsr += "Z"
-            else:
-                cpsr += "z"
-            if (globals.regs[i] & globals.CARRYBIT == globals.CARRYBIT):
-                cpsr += "C"
-            else:
-                cpsr += "c"
-            if (globals.regs[i] & globals.OVERBIT == globals.OVERBIT):
-                cpsr += "V"
-            else:
-                cpsr += "v"
-            if (globals.regs[i] & globals.QBIT == globals.QBIT):
-                cpsr += "Q"
-            else:
-                cpsr += "q"
-            if (globals.regs[i] & globals.INTIRQBIT == globals.INTIRQBIT):
-                cpsr += "I"
-            else:
-                cpsr += "i"
-            if (globals.regs[i] & globals.INTFIQBIT == globals.INTFIQBIT):
-                cpsr += "F"
-            else:
-                cpsr += "f"
-            if (globals.regs[i] & globals.THUMBBIT == globals.THUMBBIT):
-                cpsr += "T "
-            else:
-                cpsr += "t "
-            if (globals.regs[i] & globals.OPMODEBIT == int("00000017",16)):
-                cpsr += " abort"
-            if (globals.regs[i] & globals.OPMODEBIT == int("00000011",16)):
-                cpsr += " fintreq"
-            if (globals.regs[i] & globals.OPMODEBIT == int("00000012",16)):
-                cpsr += " intreq"
-            if (globals.regs[i] & globals.OPMODEBIT == int("00000013",16)):
-                cpsr += " sup"
-            if (globals.regs[i] & globals.OPMODEBIT == int("0000001f",16)):
-                cpsr += " system"
-            if (globals.regs[i] & globals.OPMODEBIT == int("0000001b",16)):
-                cpsr += " undef"
-            if (globals.regs[i] & globals.OPMODEBIT == int("00000010",16)):
-                cpsr += " user"
-        if (i == 18):
-            out += "(spsr):"
-
-        if (i == 17):
-            out += cpsr
-        return out
-
     def onPaint(self, event):
         dc = wx.PaintDC(self)
         x1,y1,w,h = dc.GetBoundingBox()
@@ -95,12 +32,12 @@ class Registers(wx.Panel):
         # convert pixels back to lines
         self.curRow = abs(y1) / self.fheight;
         dc.SetFont(self.font)
-        for i in range(self.curRow, 16):  # 16 registers
+        for i in range(self.curRow, globals.LAST_REG):  # 16 registers
             self.h = self.fheight * i - y1
             dc.SetPen(wx.Pen(wx.NamedColour('white'), 20))
             dc.DrawRectangle(0, self.h+10, w, self.fheight)
             dc.SetPen(wx.Pen(wx.NamedColour('black'), 20))
-            out = self.buildRegValString(i)
+            out = utils.buildRegValString(self, i)
             dc.DrawText(out, 5, self.h)
 
     def OnInnerSizeChanged(self):
